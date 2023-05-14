a16c328357f8353edcf0672698e086f1f0391d8d
CuriNsco <curiagus99@gmail.com>
2023-05-14 00:39:59 -0300

    al fin arregle la navbar

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -4,17 +4,17 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
 
 const fiambres = [
-  { name: 'Fiambres', current: false },
+  { name: 'Fiambres', href:'/Fiambres', current: false },
   // { name: 'Quesos', href: '/Quesos', current: false },
   // { name: 'Embutidos', href: '/Embutidos', current: false },
 ]
 
 const quesos = [
-  {name: 'Quesos', current: false},
+  {name: 'Quesos',href:'/Quesos', current: false},
 ]
 
 const embutidos= [
-  { name: 'Embutidos', current: false},
+  { name: 'Embutidos',href:'/Embutidos', current: false},
 ]
 
 function classNames(...classes) {
@@ -28,7 +28,7 @@ export default function Navbar() {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-2 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -174,6 +174,36 @@ export default function Navbar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {fiambres.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+
+{quesos.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+
+{embutidos.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as="a"
